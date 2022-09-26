4cb8363119dbfe78615411382e4cc38218a1e872
alexdonay <alexdonay@gmail.com>
2022-09-26 17:29:14 -0300

    fibonacci

--- fibonacci.py
+++ fibonacci.py
@@ -1,3 +1,5 @@
 def fibonacci(n):
-        return 1 if (n <= 2) else fibonacci(n-1) + fibonacci(n-2)
-print (fibonacci(6))
+    return 1 if (n <= 2) else fibonacci(n-1) + fibonacci(n-2)
+
+
+print(fibonacci(3))
