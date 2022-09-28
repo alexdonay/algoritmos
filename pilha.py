3e06caff3f2f299819acb1e94f9e81a3c6a1ea74
Alexsander <alexdonay@gmail.com>
2022-09-28 20:32:32 -0300

    pilhas orientaçao a objetos

--- pilha.py
+++ pilha.py
@@ -4,24 +4,37 @@ class pilhaCustomizada:
     def __init__(self, tamanho):
         self.tamanho = tamanho
 
-    def verificaVazio():
-        return True if (len(pilha)==0) else False
-    def verificaCheio():
-        return True if (len(pilha)== tamanho) else False
+    def verificaVazio(self):
+        return True if (len(self.pilha)==0) else False
+    def verificaCheio(self):
+        return True if (len(self.pilha)< self.tamanho) else False
     def adicionar(self, value):
-        if(self.verificaCheio):
+        if(self.verificaCheio()):
             self.pilha.append(value)
         else:
             print("Pilha cheia")
-    def remover():
-        if(self.verificaVazio):
+    def remover(self):
+        if(self.verificaVazio()):
             print("Pilha Vazia")
         else:
-            self.pilha.remove()
-
-pilha = pilhaCustomizada(9)
-
-
+            self.pilha.pop()
+    def toString(self):
+        return self.pilha
+pilha = pilhaCustomizada(3)
 
 pilha.adicionar(1)
+pilha.adicionar(3)
+print(pilha.toString())
+
 pilha.remover()
+print(pilha.pilha)
+pilha.remover()
+print(pilha.pilha)
+pilha.remover()
+print(pilha.pilha)
+pilha.adicionar(1)
+pilha.adicionar(3)
+print(pilha.pilha)
+pilha.adicionar(1)
+pilha.adicionar(3)
+print(pilha.pilha)
