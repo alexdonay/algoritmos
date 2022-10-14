feda558a2edb623e97b789c9d4d05a47ac9f725a
alexdonay <alexdonay@gmail.com>
2022-10-14 11:11:53 -0300

    Ajuste código
    Signed-off-by: alexdonay <alexdonay@gmail.com>

--- filapoo.py
+++ filapoo.py
@@ -1,58 +1,49 @@
 class Fila:
-    arr = []
-    tamanho = 0
-    inicio = 0
-    fim = 0
-    qtElementos = 0
-    tipo = ""
+    
+    def __init__(self, tamanhofila) -> None:
+        self.__tamanho = tamanhofila
+        self.__arr = []
+        self.__inicio = 0
+        self.__fim = 0
+        self.__qtelementos = 0
+        self.tipo = ""
 
-    def __init__(self, tamanhoFila) -> None:
-        self.tamanho = tamanhoFila
-        for i in range(self.tamanho):
-            self.arr.append("")
+        for i in range(self.__tamanho):
+            self.__arr.append("")
         
-    def Fila_Vazia(self):
-        return True if self.qtElementos == 0 else False
+    def filavazia(self):
+        return True if self.__qtelementos == 0 else False
 
-    def Fila_Cheia(self):
-        return True if self.qtElementos == self.tamanho else False
+    def filacheia(self):
+        return True if self.__qtelementos == self.__tamanho else False
 
-    def Enfileirar(self,elemento):
-        if not self.Fila_Cheia():
-            self.arr[self.inicio] = elemento
-            self.inicio = 0 if self.inicio == self.tamanho - 1 else 1 + self.inicio
-            self.qtElementos += 1
+    def enfileirar(self,elemento):
+        if not self.filacheia():
+            self.__arr[self.__inicio] = elemento
+            self.__inicio = 0 if self.__inicio == self.__tamanho - 1 else 1 + self.__inicio
+            self.__qtelementos += 1
         else:
             raise Exception("Fila está cheia")
-            
+        
+
     def desinfileirar(self):
-        if not self.Fila_Vazia():
-            self.fim = 0 if self.fim == self.tamanho-1 else self.fim + 1
-            self.qtElementos -= 1
+        if not self.filavazia():
+            item = self.__arr[self.__fim]
+            self.__fim = 0 if self.__fim == self.__tamanho-1 else self.__fim + 1
+            self.__qtelementos -= 1
+            return item
         else:
             raise Exception("A fila está vazia")
     
     def status(self):
-        print(f'inicio {self.inicio} fim {self.fim} quantidade {self.qtElementos} fila {self.arr}')
+        print(f'inicio {self.__inicio} fim {self.__fim} quantidade {self.__qtelementos} fila {self.__arr}')
 
 
 n = Fila(3)
 n.status()
-n.Enfileirar(1)
+n.enfileirar(1)
+n.enfileirar(2)
+n.enfileirar(3)
 n.status()
-n.Enfileirar(2)
-n.status()
-n.Enfileirar(3)
-n.status()
-n.desinfileirar()
-n.status()
-n.Enfileirar(5)
-n.status()
-n.desinfileirar()
-n.status()
-n.desinfileirar()
-n.status()
-n.desinfileirar()
-n.status()
-n.desinfileirar()
-n.status()
+
+print(n.desinfileirar())
