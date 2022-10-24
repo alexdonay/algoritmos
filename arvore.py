50ac70dff8634c30b768690fcb1b15abd0fe721d
alexdonay <alexdonay@gmail.com>
2022-10-24 09:06:35 -0300

    Arvore e outros
    Signed-off-by: alexdonay <alexdonay@gmail.com>

--- arvore.py
+++ arvore.py
@@ -1,13 +1,51 @@
-class Item:
-    def __init__(self, noAnterior, noAtual, valor, arvore):
-        self.noAnterior = noAnterior
-        self.noAtual = noAtual
-        self.valor = valor
-        arvore.append(self)
-    def toString(self):
-        return str(self.valor)
-    
-arvore = []
-item =  Item(0,0,1, arvore)
+class NodoArvore:
+    def __init__(self, chave=None, esquerda=None, direita=None):
+        self.chave = chave
+        self.esquerda = esquerda
+        self.direita = direita
 
-print(item.toString())
+    def __repr__(self):
+        return '%s <- %s -> %s' % (self.esquerda and self.esquerda.chave,
+                                    self.chave,
+                                    self.direita and self.direita.chave)
+def insere(raiz, nodo):
+    """Insere um nodo em uma árvore binária de pesquisa."""
+    # Nodo deve ser inserido na raiz.
+    if raiz is None:
+        raiz = nodo
+
+    # Nodo deve ser inserido na subárvore direita.
+    elif raiz.chave < nodo.chave:
+        if raiz.direita is None:
+            raiz.direita = nodo
+        else:
+            insere(raiz.direita, nodo)
+
+    # Nodo deve ser inserido na subárvore esquerda.
+    else:
+        if raiz.esquerda is None:
+            raiz.esquerda = nodo
+        else:
+            insere(raiz.esquerda, nodo)
+def em_ordem(raiz):
+    if not raiz:
+        return
+
+    # Visita filho da esquerda.
+    em_ordem(raiz.esquerda)
+
+    # Visita nodo corrente.
+    print(raiz.chave),
+
+    # Visita filho da direita.
+    em_ordem(raiz.direita)
+raiz = NodoArvore(40)
+for chave in [20, 60, 50, 70, 10, 30]:
+    nodo = NodoArvore(chave)
+    insere(raiz, nodo)
+# Imprime o caminhamento em ordem da árvore.
+
+nodo = NodoArvore(15)
+insere(raiz, nodo)
+
+em_ordem(raiz)
